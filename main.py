5d936e6569957c0a596896aa6f7cccef955557a1
Ola Krassowska <ola.krassowska@hotmail.com>
2024-06-14 14:01:40 +0200

    Renamed level.json -> levels.json and added loading maps and levels data in MainWindow and function setupUi

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import sys 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
@@ -5,15 +6,42 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("WindowTitle")
+        self.setWindowTitle("Snake")
         self.setMinimumSize(500,500)
 
-        def resizeEvent(self, event):
-            size = event.size()
-            width = self.width()
-            height = self.height()
+        self.levelsData = self.loadLevelsData()
+        self.mapsData = self.loadMapsData()
 
-            super().resizeEvent(event)
+        self.setupUi()
+
+    def loadLevelsData(self):
+        try:
+            with open("configs/levels.json", "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return []
+        except json.JSOnDecodeError:
+            return []
+        
+    def loadMapsData(self):
+        try:
+            with open("configs/maps.json", "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return []
+        except json.JSOnDecodeError:
+            return []
+            
+    def resizeEvent(self, event):
+        size = event.size()
+        width = self.width()
+        height = self.height()
+
+        super().resizeEvent(event)
+
+    def setupUi(self):
+        print(self.levelsData)
+        print(self.mapsData)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
