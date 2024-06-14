c02aa0d2d08daa652749891beecb8c326c90ba7c
Ola Krassowska <ola.krassowska@hotmail.com>
2024-06-14 14:48:52 +0200

    points and level label added, hiding labels and showing on startGame, added GridLayout, new var gameStarted instead using currentLevel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import sys 
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QGridLayout, QLabel
 from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
@@ -15,6 +15,8 @@ class MainWindow(QMainWindow):
 
         self.currentLevel = 0
         self.points = 0
+
+        self.gameStarted = False
         
         self.setupUi()
 
@@ -63,9 +65,29 @@ class MainWindow(QMainWindow):
         self.startBtn.setStyleSheet("background-color: #B3A6A3; color: #000000; border: 2px solid #968986; font-size: 25px; font-weight: semibold;")
         layout.addWidget(self.startBtn)
 
+        self.pointsLabel = QLabel()
+        self.levelLabel = QLabel()
+
+        layout.addWidget(self.pointsLabel)
+        layout.addWidget(self.levelLabel)
+
+        self.pointsLabel.hide()
+        self.levelLabel.hide()
+
+        self.gridLayout = QGridLayout()
+        layout.addLayout(self.gridLayout)
+
         self.mainWidget.setLayout(layout)
 
     def startGame(self):
+        if not self.gameStarted:
+            self.gameStarted = True
+            self.startBtn.hide()
+            self.pointsLabel.show()
+            self.levelLabel.show()
+            self.levelLabel.setText(f"level")
+            self.pointsLabel.setText(f"points")
+
         print("start game")
         pass
 
