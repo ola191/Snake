89f999e84d92387be268cb6ff878f4410ffcb4d3
Ola Krassowska <ola.krassowska@hotmail.com>
2024-06-14 14:19:12 +0200

    Added button start, set size on resize, added bg, printing error, function startGame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import sys 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -18,30 +18,50 @@ class MainWindow(QMainWindow):
         try:
             with open("configs/levels.json", "r") as file:
                 return json.load(file)
-        except FileNotFoundError:
+        except FileNotFoundError as e:
+            print(f"error {e}")
             return []
-        except json.JSOnDecodeError:
+        except json.JSOnDecodeError as e:
+            print(f"error {e}")
             return []
         
     def loadMapsData(self):
         try:
             with open("configs/maps.json", "r") as file:
                 return json.load(file)
-        except FileNotFoundError:
+        except FileNotFoundError as e:
+            print(f"error {e}")
             return []
-        except json.JSOnDecodeError:
+        except json.JSOnDecodeError as e:
+            print(f"error {e}")
             return []
             
     def resizeEvent(self, event):
         size = event.size()
-        width = self.width()
-        height = self.height()
+        width = size.width()
+        height = size.height()
+
+        self.mainWidget.setFixedSize(width, height)
 
         super().resizeEvent(event)
 
     def setupUi(self):
-        print(self.levelsData)
-        print(self.mapsData)
+        self.mainWidget = QWidget()
+        self.setCentralWidget(self.mainWidget)
+        self.mainWidget.setStyleSheet("background-color: #FFBB00;")
+
+        layout = QVBoxLayout()
+
+        startBtn = QPushButton("Start")
+        startBtn.clicked.connect(self.startGame)
+        layout.addWidget(startBtn)
+
+        self.mainWidget.setLayout(layout)
+
+    def startGame(self):
+        print("start game")
+        pass
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
